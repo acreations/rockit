18c3813c3a2a956a690da09ce21df80e19b02ce8
acreations@aeris <aaron.wong.home@gmail.com>
2016-09-04 01:09:11 +0200

    Added registration

---
+++ rockit/core/models.py
@@ -0,0 +1,31 @@
+from django.db import models
+from django.utils import timezone
+from django.utils.translation import ugettext as _
+
+
+class CreatedModel(models.Model):
+    """
+    Used to track creations of a model
+    """
+    created = models.DateTimeField(auto_now_add=True)
+    created_by = models.CharField(default='system', max_length=70, help_text=_("Who created this model"))
+
+    class Meta:
+        abstract = True
+
+
+class ModifiedModel(CreatedModel):
+    """
+    Used to track modification of a model
+    """
+
+    last_modified = models.DateTimeField(editable=False, default=timezone.now)
+    last_modified_by = models.CharField(default='system', max_length=70, help_text=_("Who modified this model"))
+
+    def save(self, *args, **kwargs):
+        self.last_modified = timezone.now()
+
+        super(ModifiedModel, self).save(*args, **kwargs)
+
+    class Meta:
+        abstract = True
--- rockit/features/registration/models.py
+++ rockit/features/registration/models.py
@@ -1,5 +1,16 @@
 from __future__ import unicode_literals
 
 from django.db import models
+from django.utils.translation import ugettext as _
 
-# Create your models here.
+from rockit.core.models import ModifiedModel
+
+
+class Hello(ModifiedModel):
+    """
+    Track a hello requests
+    """
+
+    name = models.TextField(max_length=100, help_text=_("Name of requestee"))
+    message = models.TextField(max_length=1000, help_text=_("Message of requestee"))
+    identifier = models.UUIDField(primary_key=True, unique=True, editable=False, help_text=_("Identifier of requestee"))
