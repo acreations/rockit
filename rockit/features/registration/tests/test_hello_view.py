fdfcb1b6050e0f2a30e5fb4284dcd2d97a4ce514
acreations@aeris <aaron.wong.home@gmail.com>
2016-09-05 23:20:23 +0200

    Updated tests for registration

--- rockit/features/registration/tests/test_hello_view.py
+++ rockit/features/registration/tests/test_hello_view.py
@@ -1,7 +1,43 @@
+import uuid
+
+from rest_framework import status
 from rest_framework.reverse import reverse
+from rockit.features.registration.models import Hello
 
 
 def test_it_should_be_able_to_create_hello_request(db, client):
-    response = client.get(reverse('hello-list'))
+    data = {
+        'name': 'test',
+        'message': 'test message',
+        'identifier': uuid.uuid4()
+    }
+
+    response = client.post(reverse('hello-list'), data)
 
     assert response
+    assert response.status_code is status.HTTP_201_CREATED
+
+    hellos = Hello.objects.all()
+
+    assert len(hellos) is 1
+
+
+def test_it_should_not_be_possible_to_create_more_than_one_hello(db, client):
+    data = {
+        'name': 'test',
+        'message': 'test message',
+        'identifier': uuid.uuid4()
+    }
+
+    response_one = client.post(reverse('hello-list'), data)
+
+    response_two = client.post(reverse('hello-list'), data)
+
+    assert response_one
+    assert response_one.status_code is status.HTTP_201_CREATED
+    assert response_two
+    assert response_two.status_code is not status.HTTP_201_CREATED
+
+    hellos = Hello.objects.all()
+
+    assert len(hellos) is 1
